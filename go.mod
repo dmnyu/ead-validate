20702f48e69ef133b6dbf8a84103bd05a395a94c
Donald <don.mennerich@nyu.edu>
2021-09-13 10:13:51 -0400

    adding validation logic

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/dmnyu/ead-validate
 
 go 1.16
 
-require fyne.io/fyne/v2 v2.0.4
+require (
+	fyne.io/fyne/v2 v2.0.4
+	github.com/nyudlts/go-aspace v0.0.0-00010101000000-000000000000
+)
+
+replace github.com/nyudlts/go-aspace => /Users/menneric/go/src/github.com/nyudlts/go-aspace
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"github.com/nyudlts/go-aspace"
 	"image/color"
+	"io/ioutil"
+	"os"
 )
 
 var black = color.Black
@@ -15,7 +19,7 @@ var black = color.Black
 func main() {
 	a := app.NewWithID("edu.nyu.dlts.ead-validate")
 	validator := a.NewWindow("EAD Validator")
-	validator.Resize(fyne.NewSize(480,480))
+	validator.Resize(fyne.NewSize(480, 480))
 	terminal := buildTerminal()
 	fileEntry := buildFileEntry(terminal)
 	c := container.New(layout.NewVBoxLayout(), fileEntry, terminal)
@@ -35,8 +39,51 @@ func buildFileEntry(terminal *fyne.Container) *fyne.Container {
 	entry := widget.NewEntry()
 	c.Add(entry)
 	button := widget.NewButton("Validate", func() {
-		terminal.Add(canvas.NewText("Test", black))
+		path := entry.Text
+		terminal.Add(canvas.NewText(fmt.Sprintf("Validating `%s`", path), black))
+		if fileExists(path) == true {
+			terminal.Add(canvas.NewText(fmt.Sprintf("  `%s` exists", path), black))
+			if isFile(path) == true {
+				terminal.Add(canvas.NewText(fmt.Sprintf("  `%s` is a file", path), black))
+				if isValid(path) == true {
+					terminal.Add(canvas.NewText(fmt.Sprintf("  `%s` is a valid", path), black))
+				} else {
+					terminal.Add(canvas.NewText(fmt.Sprintf("  `%s` is not valid", path), black))
+				}
+			} else {
+				terminal.Add(canvas.NewText(fmt.Sprintf("  `%s` is a directory, cannot validate", path), black))
+			}
+		} else {
+			terminal.Add(canvas.NewText(fmt.Sprintf("`%s` does not exist", path), black))
+		}
 	})
 	c.Add(button)
 	return c
+}
+
+func fileExists(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		return true
+	} else if err != nil {
+		return false
+	} else {
+		return false
+	}
+}
+
+func isFile(path string) bool {
+	fi, _ := os.Stat(path)
+	if fi.IsDir() {
+		return false
+	}
+	return true
+}
+
+func isValid(path string) bool {
+	f, _ := ioutil.ReadFile(path)
+	err := aspace.ValidateEAD(f)
+	if err != nil {
+		return false
+	}
+	return true
 }
